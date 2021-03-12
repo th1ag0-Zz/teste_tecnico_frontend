34ed7a17797724eaf00b0c2fa9e5eee5a5b1e837
Thiago <thiagoF89.tf@gmail.com>
2021-03-12 09:53:21 -0300

    :tada: add charts :tada:

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,123 @@
-import '../src/styles/global.css'
 import Header from './components/Header'
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+import '../src/styles/global.css'
+import styles from './styles/components/App.module.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+
+  window.addEventListener('resize', function(){
+    setWindowWidth(window.innerWidth);
+  });
+
+  useEffect(() => {
+    if (windowWidth < 940) {
+      setPercentWidth(90)
+    } else {
+      setPercentWidth(48)
+    }
+  }, [windowWidth])
+
+  const [percentWidth, setPercentWidth] = useState(48)
+
+  const data = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+
   return (
     <div>
       <Header></Header>
+
+      <div className={styles.chartContainer}>
+        <ResponsiveContainer width={`${percentWidth}%`} height="70%">
+          <BarChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="pv" fill="#00c9f6" />
+            <Bar dataKey="uv" fill="#ff5043" />
+          </BarChart>
+        </ResponsiveContainer>
+
+        <ResponsiveContainer width={`${percentWidth}%`} height="70%">
+          <BarChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="pv" fill="#00c9f6" />
+            <Bar dataKey="uv" fill="#ff5043" />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 }
