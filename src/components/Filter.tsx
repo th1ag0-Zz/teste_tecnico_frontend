f9c4167a6f806d8516a6b0f3386be9f35bd94e79
Thiago <thiagoF89.tf@gmail.com>
2021-03-18 12:29:37 -0300

    :tada: fix: erro de deploy :tada:

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect } from 'react';
+import { useState, useContext } from 'react';
 import { DataContext } from '../contexts/DataContext';
 import styles from '../styles/components/Filter.module.css';
 
